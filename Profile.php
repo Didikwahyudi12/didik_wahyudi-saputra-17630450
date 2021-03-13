f57bc0425748cf8d5f548bd6ebcae74731768f68
ikhlasulamal140345 <Didikwahyudisaputra@gmail.com>
2021-03-14 04:56:08 +0700

    Project praktikum Didik Wahyudi Saputra 17630450

---
+++ Profile.php
@@ -0,0 +1,54 @@
+<?php include "header.php" ?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/D3.jpg" alt="Didik Wahyudi Saputra" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NPM :</strong></td>
+                        <td align="left" width="50%">6212011505960003 / 17630450</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Didik Wahyudi Saputra</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Bahitom, 15 Mei 1996</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0852 4886 4398</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">didikwahyudisaputra@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/Didikwahyudisaputra" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/036-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/Didikwahyudi_s" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/Didikwahyudi__S" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/008-twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/Didik wahyudi" target="_blank">
+                                <img src="img/svg/027-linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
